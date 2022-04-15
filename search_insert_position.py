07c45b1b2fa1ed94bb808a9b1868856ed64d8d7f
Yodsaphon Sawatdiwong <65103515+GapYodsaphon@users.noreply.github.com>
2022-04-15 20:53:01 +0700

    add search_insert_position ver. binarysearch

--- search_insert_position.py
+++ search_insert_position.py
@@ -14,42 +14,43 @@ Runtime: 84 ms
 Memory Usage: 14.7 MB
 """
 
+# Normal
 
-def searchInsert (nums, target) :
 
-    l,r = 0,len(nums)-1
+def searchInsert(nums, target):
+
+    l, r = 0, len(nums)-1
 
     insert_loc = 0
-    while l<r :
-        if nums[l]==target:
-            return l 
-        elif nums[r]==target:
+    while l < r:
+        if nums[l] == target:
+            return l
+        elif nums[r] == target:
             return r
-        l+=1
-        r-=1
+        l += 1
+        r -= 1
 
     for i in range(len(nums)):
-        if nums[i]<target:
+        if nums[i] < target:
             insert_loc = i+1
 
     return insert_loc
 
-print(searchInsert([1,3,5,6],0))
+
+print(searchInsert([1, 3, 5, 6], 0))
 
 
-"""
-Fast Solution
-min = 0
-max = len(nums)-1  
-        while min <= max:
-            mid = (min+max)//2 #rounds down
-            if target == nums[mid]:
-                return mid
-            else:
-                if target < nums[mid]:
-                    max = mid-1
-                else:
-                    min = mid+1
-return min      
+# ----> Binary Search Way
+def searchInsertBinary(nums, target):
+    left, right = 0, len(nums)-1
+    while left <= right:
+        m = (left+right)//2
+        if nums[m] == target:
+            return m
+        elif target > nums[m]:
+            left = m+1
+        elif target < nums[m]:
+            right = m-1
+    return left
 
-"""
+print(searchInsertBinary([1, 3, 5, 6], 0))
