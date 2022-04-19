f10eaab9b7ff7e40d7f05f4dc3cbb01f24b71feb
Yodsaphon Sawatdiwong <65103515+GapYodsaphon@users.noreply.github.com>
2022-04-19 20:04:08 +0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,13 +1,14 @@
 def sort(arr):
-    for i in range(len(arr)-1):
+    for i in range(len(arr)):
         # check ทีละ index
-        for j in range(len(arr)-i-1):
+        # แต่ละ i จะ ได้ 1 ตัว ซ้ายสุดที่มากสุดและลำดับไม่เปลี่ยนละ
+        for j in range(len(arr)-1-i):
+            
             if arr[j] > arr[j+1]:
                 tmp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = tmp
-        
     return arr
 
 
-print(sort([4,5,9,2,7,1]))
+print(sort([4,5,222,9,2,7,1]))
